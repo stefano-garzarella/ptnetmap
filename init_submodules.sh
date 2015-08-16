557c3d6f795a15404b03e361c1a5507955e4900d
Stefano Garzarella <stefano.garzarella@gmail.com>
2015-08-16 09:47:53 +0200

    add bhyve submodule

--- init_submodules.sh
+++ init_submodules.sh
@@ -11,6 +11,7 @@ if [ x"$DIR_REPOS" != x ]; then
     git submodule update --reference ${DIR_REPOS}/linux -- linux
     git submodule update --reference ${DIR_REPOS}/qemu -- qemu
     git submodule update --reference ${DIR_REPOS}/freebsd -- freebsd
+    git submodule update --reference ${DIR_REPOS}/bhyve -- bhyve
 else
     git submodule update --init —recursive
 fi
--- submodules_checkout.sh
+++ submodules_checkout.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-BRANCH="ptnetmap_memdev"
-SUB_GIT="netmap
-linux
-qemu
-freebsd"
+BRANCH="ptnetmap"
+SUB_GIT="linux
+qemu"
+
+set -x
+for d in $SUB_GIT
+do
+    cd "$d"
+    git fetch
+    git checkout -b $BRANCH origin/$BRANCH
+    cd -
+done
+
+BRANCH="ptnetmap10"
+SUB_GIT="freebsd
+bhyve"
 
 set -x
 for d in $SUB_GIT
